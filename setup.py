7238c9ff5f35baf5d2182c953d41f2d40d08693d
H4CKY54CK <tk13xr37@gmail.com>
2020-03-05 02:52:31 -0500

    Loose ends
    Specified minimum python version requirement, as well as incremented our own version.

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ def readme():
         return f.read()
 
 setup(name='misctools',
-      version='1.3',
+      version='1.4',
       description="'Miscellaneously Incredible Suite of Cool' TOOLS",
       long_description=readme(),
       author='Hackysack',
       author_email='tk13xr37@gmail.com',
       packages=['misctools'],
       install_requires=['progressbar2', 'pillow', 'praw'],
+      python_requires='>=3.6',
       entry_points={'console_scripts':
           ['spriteit = misctools.spriteit:main',
           'zipit = misctools.zipit:main',
