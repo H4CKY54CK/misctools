2bf51c1f053c94963d4298bf1fcc1c093ac9a25a
Hackysack <tk13xr37@gmail.com>
2020-08-28 17:30:55 -0400

    Cleanup/Update

--- misctools/__init__.py
+++ misctools/__init__.py
@@ -1,3 +1,4 @@
-__version__ = "2.0.1"
+__version__ = "2.1.0"
 
 from .timer import *
+from .utils import walk
--- misctools/utils.py
+++ misctools/utils.py
@@ -2,6 +2,66 @@ import os
 import shutil
 import sys
 import argparse
+import begin
+from glob import glob
+from urllib.request import urlopen
+
+sprint = sys.stdout.write
+
+@begin.start
+def wcit(*filenames):
+    
+    files = [f for g in filenames for f in glob(g)]
+    chars = 0
+    words = 0
+    lines = 0
+    system_dependent_shorthand = '\n' if os.name == 'nt' else ''
+    output = []
+    for file in files:
+        try:
+            with open(file, errors='ignore') as f:
+                d = f.read()
+            l = len(d.split('\n'))-1
+            w = len(d.split())
+            c = os.path.getsize(file)
+            chars += c
+            words += w
+            lines += l
+            output.append((l, w, c, file))
+        except:
+            pass
+    if len(output) > 2:
+        output.append((lines, words, chars, 'total'))
+    w = max([len(str(i)) for i in (chars,words,lines)])
+    ww = max([len(str(i)) for i in (files)])
+    if w < 5:
+        w = 5
+    for item in output:
+        sprint('{:>{width}} {:>{width}} {:>{width}} {:>{wwidth}}\n'.format(*item,width=w,wwidth=ww))
+
+
+@begin.start
+def wgetit(url, name=None):
+    if name is None:
+        name = os.path.split(url)[1]
+    try:
+        with open(name, 'wb') as f:
+            f.write(urlopen(url).read())
+            return f"{url} -> {name}"
+    except Exception as e:
+        return str(e)
+
+def walk(path):
+    d = [path]
+    files = []
+    while d:
+        for item in os.scandir(d[0]):
+            if item.is_dir():
+                d.append(item.path)
+            else:
+                files.append(item.path)
+        d.pop(0)
+    return files
 
 def search(args):
 
@@ -24,7 +84,6 @@ def search(args):
         for filename in filenames:
             if any(i.lower() in filename.lower() for i in args.files):
                 print(os.path.abspath(os.path.join(root, filename)))
-
 
 def main(argv=None):
     argv = (argv or sys.argv)[1:]
